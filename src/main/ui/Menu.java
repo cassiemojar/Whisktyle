8547f8390f6f338c431ab67596687e79e71a00a4
Cassandra <cassiemojar09@gmail.com>
2025-07-25 13:49:41 -0700

    Added title header to UI

--- src/main/ui/Menu.java
+++ src/main/ui/Menu.java
@@ -13,6 +13,7 @@ public class Menu extends JFrame {
     // EFFECTS: Constructor for the main menu, initializes all model classes
     public Menu() {
         super("Cher's Closet");
+        closet = new Closet();
         setupFrame();
 
     }
@@ -22,17 +23,20 @@ public class Menu extends JFrame {
         setTitle("Cher's Closet");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //setSize(400, 400);
-        setResizable(false);
+        // setSize(1000, 500);
+        setResizable(true);
         setLayout(new BorderLayout());
+
         setBackgroundImage();
+
         setVisible(true);
 
     }
 
+    // EFFECTS: Creates background image icon then paints it to the entire screen
     public void setBackgroundImage() {
         setContentPane(new JPanel() {
-            private Image bg = new ImageIcon(getClass().getResource("images/background.png")).getImage();
+            private Image bg = new ImageIcon(getClass().getResource("img/background.png")).getImage();
 
             // EFFECTS: Overrides the paintComponent so that it can paint the backgground
             // image
@@ -40,7 +44,29 @@ public class Menu extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g); // Paints back
                 g.drawImage(bg, 0, 0, getWidth(), getHeight(), this); // Draws image
+
             }
+
         });
+
+        setUI();
+
+        setVisible(true);
     }
+
+    public void setUI() {
+        JPanel panel = new JPanel();
+        ImageIcon headerImg = new ImageIcon(getClass().getResource("img/whisktyle.png"));
+        Image image = headerImg.getImage().getScaledInstance(456, 134, Image.SCALE_SMOOTH);
+
+        headerImg = new ImageIcon(image);
+        JLabel header = new JLabel(headerImg);
+
+        header.setHorizontalAlignment(JLabel.CENTER);
+        header.setVerticalAlignment(JLabel.TOP);
+
+        panel.add(header);
+        add(panel);
+    }
+
 }
