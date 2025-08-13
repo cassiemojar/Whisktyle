2daaf1babeb14bdb983ada9661cdf9fbbbaa0134
Cassandra <cassiemojar09@gmail.com>
2025-08-13 16:09:38 -0700

    Added json files (nothing written yet)

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,5 +1,10 @@
 package persistence;
 
 public class JsonReader {
+    private String source;
 
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+        this.source = source;
+    }
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,14 @@
 package persistence;
 
-public class JsonWriter {
+import java.io.PrintWriter;
 
+public class JsonWriter {
+    //private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
 }
